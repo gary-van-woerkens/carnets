5d9e2dad4dd54fea5324c1cfd970bec115dfa334
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-11-12 03:40:36 +0100

    chore: Remove useless icon file

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -12,65 +12,59 @@ const Post = ({
   term,
   needs,
   author,
+  editable,
   team_slug,
   created_at,
   priorities,
-  editable,
   handlePostDeletion,
-}: Post): JSX.Element => {
-  return (
-    <article className="post">
-      <div className="flex mb-5">
-        <div className="flex-1">
-          <h2>{team?.name}</h2>
-          <div className="flex text-sm text-gray-500 items-end">
-            <p>
-              Publié le{" "}
-              {created_at && format(new Date(created_at), "dd/MM/yyyy")}{" "}
-              par&nbsp;
-            </p>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={`https://github.com/${author}`}
-            >
-              {author}
-            </a>
-          </div>
-        </div>
-        <Mood mood={mood} />
-      </div>
-      <TabPanel>
-        <Tabs>
-          <Tab disabled={!priorities.length}>Piorités</Tab>
-          <Tab disabled={!needs.length}>Besoins</Tab>
-          <Tab disabled={!term.length}>Échéances</Tab>
-        </Tabs>
-        <Panels>
-          <ReactMarkdown className="prose prose-sm">{priorities}</ReactMarkdown>
-          <ReactMarkdown className="prose prose-sm">{needs}</ReactMarkdown>
-          <ReactMarkdown className="prose prose-sm">{term}</ReactMarkdown>
-        </Panels>
-      </TabPanel>
-      {editable && (
-        <div className="flex justify-end">
-          <Link href={`/team/${team_slug}/post/${id}`}>
-            <a className="btn primary">
-              <i className="ri-edit-fill ri-fw" />
-              Editer
-            </a>
-          </Link>
-          <button
-            className="primary ml-2"
-            onClick={() => handlePostDeletion(id)}
+}: Post) => (
+  <article className="post">
+    <div className="flex mb-5">
+      <div className="flex-1">
+        <h2>{team?.name}</h2>
+        <div className="flex text-sm text-gray-500 items-end">
+          <p>
+            Publié le {created_at && format(new Date(created_at), "dd/MM/yyyy")}{" "}
+            par&nbsp;
+          </p>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://github.com/${author}`}
           >
-            <i className="ri-delete-bin-7-fill ri-fw" />
-            Supprimer
-          </button>
+            {author}
+          </a>
         </div>
-      )}
-    </article>
-  )
-}
+      </div>
+      <Mood mood={mood} />
+    </div>
+    <TabPanel>
+      <Tabs>
+        <Tab disabled={!priorities.length}>Piorités</Tab>
+        <Tab disabled={!needs.length}>Besoins</Tab>
+        <Tab disabled={!term.length}>Échéances</Tab>
+      </Tabs>
+      <Panels>
+        <ReactMarkdown className="prose prose-sm">{priorities}</ReactMarkdown>
+        <ReactMarkdown className="prose prose-sm">{needs}</ReactMarkdown>
+        <ReactMarkdown className="prose prose-sm">{term}</ReactMarkdown>
+      </Panels>
+    </TabPanel>
+    {editable && (
+      <div className="flex justify-end">
+        <Link href={`/team/${team_slug}/post/${id}`}>
+          <a className="btn primary">
+            <i className="ri-edit-fill ri-fw" />
+            Editer
+          </a>
+        </Link>
+        <button className="primary ml-2" onClick={() => handlePostDeletion(id)}>
+          <i className="ri-delete-bin-7-fill ri-fw" />
+          Supprimer
+        </button>
+      </div>
+    )}
+  </article>
+)
 
 export default Post
